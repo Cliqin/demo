9a822a297830b43f433c59fa0cc9f97b085058b8
Cliqin <104069156+Cliqin@users.noreply.github.com>
2023-01-10 18:22:18 +0800

    Update clockIn_gzhu.py

--- clockIn_gzhu.py
+++ clockIn_gzhu.py
@@ -192,6 +192,7 @@ login?service=https%3A%2F%2Fnewmy.gzhu.edu.cn%2Fup%2Fview%3Fm%3Dup"
             self.driver.find_element(By.XPATH, xpath).click()
 
         # reviews
+        time.sleep(2)
         self.wdwait.until(
             EC.visibility_of_element_located(
                 (By.XPATH, "//div[@class='dialog_content']")
@@ -200,26 +201,26 @@ login?service=https%3A%2F%2Fnewmy.gzhu.edu.cn%2Fup%2Fview%3Fm%3Dup"
         message = self.driver.execute_script(
             "return document.getElementsByClassName('dialog_content')[0]['textContent']"
         )
-        print(message)
-        if 'reviews' in message:
+        logger.info(message)
+        if 'reviews' or '备注' in message:
             self.wdwait.until(
                 EC.element_to_be_clickable(
                     (By.XPATH, "//button[@class='dialog_button default fr']")
                 )
             ).click()
+            time.sleep(2)
             self.wdwait.until(
                 EC.visibility_of_element_located(
                     (By.XPATH, "//div[@class='dialog_content']")
                 )
             )
-            time.sleep(2)
             message = self.driver.execute_script(
                 "return document.getElementsByClassName('dialog_content')[0]['textContent']"
             )
 
-        print(message)
+        logger.info(message)
 
-        if message == "办理成功!":
+        if message == "Done successfully!" or "办理成功!":
             logger.info("健康打卡成功")
         else:
             logger.error(f"弹出框消息不正确，为:{message}")
